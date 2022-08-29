c4ea31dde534a073bafea667342dadde6583a69f
Carla Oliveira <54954245+carlasoliveira@users.noreply.github.com>
2022-08-29 19:59:14 -0300

    Correções na criação da tabela
    Correções na criação da tabela

--- aula-22-08.sql
+++ aula-22-08.sql
@@ -9,19 +9,19 @@ DROP DATABASE servicos_agricolas;
 
 -- Ao tentar executar o comando 'DROP DATABASE servicos_agricolas' novamente, é exibida uma mensagem de erro, visto que o banco de dados foi excluído anteriormente e, portanto, não pode ser excluído.
 
-DROP DATABASE IF EXISTS servicos_agricolas
+DROP DATABASE IF EXISTS servicos_agricolas;
 
 -- Quando o comando 'DROP DATABASE IF EXISTS servicos_agricolas' foi executado, foi exibido um alerta, indicando que não foi possível excluir o banco de dados, pois ele não existe (havia sido excluído anteriormente). A diferença entre DROP DATABASE e DROP DATABASE IF EXISTS é que o acréscimo de IF EXISTS fará que seja verificado SE EXISTE o banco de dados antes de tentar excluí-lo. Se existir, o banco será excluído; se não, será exibido um alerta indicando que não foi possível realizar a exclusão, diferentemente do DROP DATABASE que força a exclusão do banco e, quando não é possível, gera um erro.
 
-CREATE TABLE servicos_agricolas
+CREATE DATABASE servicos_agricolas;
 
-USE servicos_agricolas
+USE servicos_agricolas;
 
 CREATE TABLE cliente(
-    id_cliente INT NOT NULL PRIMARY KEY,
+    id_cliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     data_nasc DATE NOT NULL,
-    cpf NUMERIC(11) NOT NULL,
+    cpf NUMERIC(11) NOT NULL UNIQUE,
     telefone NUMERIC(11) NOT NULL,
     email VARCHAR(50) NOT NULL
 );
